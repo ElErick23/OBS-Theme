8bfd569d25d580916255b48307195f0ff96539c6
Erick <erickcrzc@gmail.com>
2021-05-19 17:05:01 -0500

    Hardcoded neon theme

---
+++ back.js
@@ -0,0 +1,12 @@
+// Please use event listeners to run functions.
+// document.addEventListener('onLoad', function(obj) {
+window.addEventListener('load', function(obj) {
+    // obj will be empty for chat widget
+	// this will fire only once when the widget loads
+    // const chatlist_item = document.getElementById("chatlist_item");
+});
+
+document.addEventListener('onEventReceived', function(obj) {
+  	// obj will contain information about the event
+	
+});
